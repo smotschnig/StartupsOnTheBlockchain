71cc2973d03397888d737cf6d51dbfeac074678a
Sven Motschnig <sven@motschnig.de>
2018-06-04 16:54:06 +0200

    Added Testfile

---
+++ test/Project.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+const compiledFactory = require('../ethereum/build/ProjectFactory.json');
+const compiledProject = require('../ethereum/build/Project.json');
+
+let accounts;
+let factory;
+let projectAddress;
+let project;
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+
+    factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+        .deploy({ data: compiledFactory.bytecode })
+        .send({ from: accounts[0], gas: '1000000' });
+});
