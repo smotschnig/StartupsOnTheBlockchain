765ca65b0615fc8d2c21942c6d5d80cfc17f5d1c
Sven Motschnig <sven@motschnig.de>
2018-07-11 14:36:55 +0200

    changing to production

--- server.js
+++ server.js
@@ -1,16 +1,47 @@
-const { createServer } = require('http');
+// const { createServer } = require('http');
+// const next = require('next');
+
+// const app = next({
+//     dev: process.env.NODE_ENV !== 'production'
+// });
+
+// const routes = require('./routes');
+// const handler = routes.getRequestHandler(app);
+
+// app.prepare().then(() => {
+//     createServer(handler).listen(3000, (err) => {
+//         if (err) throw err;
+//         console.log('Ready on localhost:3000');
+//     });
+// });
+
+const express = require('express');
 const next = require('next');
+const { parse } = require('url');
 
-const app = next({
-    dev: process.env.NODE_ENV !== 'production'
-});
+const dev = process.env.NODE_ENV !== 'production';
+const PORT = process.env.PORT || 3000;
 
-const routes = require('./routes');
-const handler = routes.getRequestHandler(app);
+const app = next({ dir: '.', dev });
+const handle = app.getRequestHandler();
 
+const getRoutes = require('./routes');
+
+const routes = getRoutes;
 app.prepare().then(() => {
-    createServer(handler).listen(3000, (err) => {
+    const server = express();
+    server.get('*', (req, res) => {
+        const parsedUrl = parse(req.url, true);
+        const { pathname, query = {} } = parsedUrl;
+        const route = routes[pathname];
+        if (route) {
+            return app.render(req, res, route.page, query);
+        }
+        return handle(req, res);
+    });
+
+    server.listen(PORT, (err) => {
         if (err) throw err;
-        console.log('Ready on localhost:3000');
+        console.log(`> Ready on http://localhost:${PORT}`);
     });
 });
