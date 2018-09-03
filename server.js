407fa1d76928d9c06b2d3fd3ee4b5b7be1c3904a
Sven Motschnig <sven@motschnig.de>
2018-09-03 13:14:23 +0200

    heroku testing

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
-// const express = require('express');
-// const next = require('next');
-// const { parse } = require('url');
+const express = require('express');
+const next = require('next');
+const { parse } = require('url');
 
-// const dev = process.env.NODE_ENV !== 'production';
-// const PORT = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== 'production'
+const PORT = process.env.PORT || 3000;
 
-// const app = next({ dir: '.', dev });
-// const handle = app.getRequestHandler();
+const app = next({ dir: '.', dev });
+const handle = app.getRequestHandler();
 
-// const routes = require('./routes');
+const routes = require('./routes');
 
-// app.prepare().then(() => {
-//     const server = express();
-//     server.get('*', (req, res) => {
-//         const parsedUrl = parse(req.url, true);
-//         const { pathname, query = {} } = parsedUrl;
-//         const route = routes[pathname];
-//         if (route) {
-//             return app.render(req, res, route.page, query);
-//         }
-//         return handle(req, res);
-//     });
+app.prepare().then(() => {
+    const server = express();
+    server.get('*', (req, res) => {
+        const parsedUrl = parse(req.url, true);
+        const { pathname, query = {} } = parsedUrl;
+        const route = routes[pathname];
+        if (route) {
+            return app.render(req, res, route.page, query);
+        }
+        return handle(req, res);
+    });
 
-//     server.listen(PORT, (err) => {
-//         if (err) throw err;
-//         console.log(`> Ready on http://localhost:${PORT}`);
-//     });
-// });
+    server.listen(PORT, (err) => {
+        if (err) throw err;
+        console.log(`> Ready on http://localhost:${PORT}`);
+    });
+});
 
 
 ////////////////////////////////////////////////////
@@ -48,31 +48,31 @@
 // });
 ////////////////////////////////////////////////////
 
-const { createServer } = require('http');
-const next = require('next');
+// const { createServer } = require('http');
+// const next = require('next');
 
-const app = next({
-    dev: process.env.NODE_ENV !== 'production',
-    conf: {
-        webpack: config => {
-            config.devtool = false;
+// const app = next({
+//     dev: process.env.NODE_ENV !== 'production',
+//     conf: {
+//         webpack: config => {
+//             config.devtool = false;
 
-            for (const r of config.module.rules) {
-                if (r.loader === 'babel-loader') {
-                    r.options.sourceMaps = false;
-                }
-            }
-            return config;
-        }
-    }
-});
+//             for (const r of config.module.rules) {
+//                 if (r.loader === 'babel-loader') {
+//                     r.options.sourceMaps = false;
+//                 }
+//             }
+//             return config;
+//         }
+//     }
+// });
 
-const routes = require('./routes');
-const handler = routes.getRequestHandler(app);
+// const routes = require('./routes');
+// const handler = routes.getRequestHandler(app);
 
-app.prepare().then(() => {
-    createServer(handler).listen(3000, err => {
-        if (err) throw err;
-        console.log('Ready on localhost:3000');
-    });
-});
+// app.prepare().then(() => {
+//     createServer(handler).listen(3000, err => {
+//         if (err) throw err;
+//         console.log('Ready on localhost:3000');
+//     });
+// });
